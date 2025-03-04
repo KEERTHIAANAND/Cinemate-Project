d6ded60ab59d1bbe338cdabddc2a28824319add7
KEERTHIAANAND <keerthiaanand77@gmail.com>
2025-03-04 21:42:54 +0530

    started

---
+++ src/routes/AllRoutes.js
@@ -0,0 +1,7 @@
+import { Routes,Route} from 'react-router-dom';
+import { MovieList, MovieDetail,Search,PageNotFound } from '../pages';
+export const AllRoutes = () => {
+  return (
+    <div>AllRoutes</div>
+  )
+}
