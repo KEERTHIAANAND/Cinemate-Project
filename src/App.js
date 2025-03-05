82bca74a3c22d7872fc519aa24c12244cbf353e0
KEERTHIAANAND <keerthiaanand77@gmail.com>
2025-03-06 00:54:07 +0530

    header and footer started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { AllRoutes } from './routes/AllRoutes';
+import{Header,Footer} from "./components/";
 
 export default function App() {
   return (
     <div>
+      <Header/>
       <AllRoutes/>
+      <Footer/>
     </div>
   )
 }
